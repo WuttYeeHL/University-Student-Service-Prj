230750caaffaa90b403d2ec4156ffe5ad2da6929
Khin LeLe <KhinLe@LTEDP123.ptc.com.sg>
2025-05-04 00:36:34 +1200

    Added Student info UI 1.0

--- university-student-service/frontend/student-portal/src/app/components/top-bar/top-bar.component.ts
+++ university-student-service/frontend/student-portal/src/app/components/top-bar/top-bar.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { UserService } from '../../services/user.service';
-import { User } from '../../model/interface/user';
 
 @Component({
   selector: 'app-top-bar',
@@ -9,9 +7,6 @@ import { User } from '../../model/interface/user';
   styleUrl: './top-bar.component.scss',
 })
 export class TopBarComponent {
-  user: User;
-
-  constructor(private userService: UserService) {
-    this.user = this.userService.getUser();
-  }
+  userImageUrl = 'assets/profile.jpg'; // Replace with real user image
+  userEmail = 'user@example.com'; // Replace with dynamic email
 }
