da1c6db8fd7a957174a29c8f2908aa69784a2afb
khinlele <33617584+khinlele@users.noreply.github.com>
2025-05-14 23:11:09 +1200

    Update Program.cs

--- university-student-service/backend/StudentService/Program.cs
+++ university-student-service/backend/StudentService/Program.cs
@@ -4,7 +4,7 @@ using Amazon.S3;
 using StudentService;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
