94daf7904a12cf4cde0237b30bbd5c1a7aab56de
Khin LeLe <KhinLe@LTEDP123.ptc.com.sg>
2025-05-14 23:57:44 +1200

    added program.cs

--- university-student-service/backend/StudentService/Program.cs
+++ university-student-service/backend/StudentService/Program.cs
@@ -5,6 +5,7 @@ using StudentService;
 var builder = WebApplication.CreateBuilder(args);
 
 //// Add services to the container.
+//test
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
